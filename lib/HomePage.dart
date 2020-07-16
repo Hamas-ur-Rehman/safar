e95370f6a7d444ddf40a022f5195877043b254e9
Hamas-ur-Rehman <47780362+Hamas-ur-Rehman@users.noreply.github.com>
2020-07-16 15:28:45 +0500

    commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -522,3 +522,10 @@ class _HomePageState extends State<HomePage> {
 }
 
 // }
+class HexToColor extends Color {
+  static _hexToColor(String code) {
+    return int.parse(code.substring(1, 7), radix: 16) + 0xFF000000;
+  }
+
+  HexToColor(final String code) : super(_hexToColor(code));
+}
